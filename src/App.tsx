c98bdac15e6d7fcbd5d339047f1f5dcd4090d976
FARUK2059 <omarfaruk34552@gmail.com>
2025-01-31 11:27:43 +0600

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ const App = () => {
 
     return (
         <div>
-            <div className="text-4xl text-center  text-blue-400"> The Component is Start </div>
+            <div className="text-4xl text-center text-blue-400"> The Component is Start </div>
         </div>
     );
 };
---
+++ src/routes/routes.tsx
@@ -0,0 +1,37 @@
+import { createBrowserRouter } from "react-router";
+import App from "../App";
+
+
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+    },
+    // {
+    //     path: '/admin',
+    //     element: <App />,
+    //     children: routeGenerator(adminPaths),
+    // },
+    // {
+    //     path: '/faculty',
+    //     element: <App />,
+    //     children: routeGenerator(facultyPaths),
+    // },
+    // {
+    //     path: '/student',
+    //     element: <App />,
+    //     children: routeGenerator(studentPaths),
+    // },
+
+    // {
+    //     path: '/login',
+    //     element: <Login />,
+    // },
+    // {
+    //     path: '/register',
+    //     element: <Register />,
+    // },
+]);
+
+export default router;
