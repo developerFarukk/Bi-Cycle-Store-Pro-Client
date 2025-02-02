11d2a694e68e57429450f9377f24298d9e3cc2f2
FARUK2059 <omarfaruk34552@gmail.com>
2025-02-02 20:31:58 +0600

    add card 4756

--- src/shared/Navbar.tsx
+++ src/shared/Navbar.tsx
@@ -5,6 +5,7 @@ import Ridenest from "./Ridenest";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { logout } from "@/redux/features/auth/authSlice";
 import { toast } from "sonner";
+import { RootState } from "@/redux/store";
 
 
 
@@ -14,6 +15,9 @@ const Navbar = () => {
     const dispatch = useAppDispatch();
     const { token } = useAppSelector((state) => state.auth);
     // console.log(token);
+    const cartItems = useAppSelector((state: RootState) => state.cart);
+
+    // console.log(cartItems.totalQuantity);
 
     const handleLogout = () => {
         const toastId = toast.loading("Logging out...");
@@ -136,11 +140,11 @@ const Navbar = () => {
 
                         <div className="flex justify-center items-center md:block">
 
-                            <Link to="/">
+                            <Link to="/customer/mycart">
                                 <div className=" flex justify-center items-center text-center">
                                     <div className="relative ">
                                         <div className="t-0 absolute left-3">
-                                            <p className="flex h-1 w-1 items-center justify-center rounded-full bg-fuchsia-600 p-3 text-xs text-black">3</p>
+                                            <p className="flex h-1 w-1 items-center justify-center rounded-full bg-fuchsia-600 p-3 text-xs text-black">{cartItems.totalQuantity}</p>
                                         </div>
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="file: mt-4 h-5 w-5">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
