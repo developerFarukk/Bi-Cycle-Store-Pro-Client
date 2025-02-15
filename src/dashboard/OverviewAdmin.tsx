72c1b18c188cc5ed02aed61dbfbcb6bc3f10909d
FARUK2059 <omarfaruk34552@gmail.com>
2025-02-15 20:46:42 +0600

    udate dashboard

--- src/dashboard/OverviewAdmin.tsx
+++ src/dashboard/OverviewAdmin.tsx
@@ -1,14 +1,32 @@
+import { useGetAllOrdersQuery } from "@/redux/features/orderManagmentApi/OrderManagmentApi";
+import { useGetAllUsersQuery } from "@/redux/features/userManagment/userManagmentApi";
 
 
 
 const OverviewAdmin = () => {
 
+    const { data: userData } = useGetAllUsersQuery(undefined);
+    const totalUser = userData?.data?.meta?.total;
+
+    const { data: orderData } = useGetAllOrdersQuery(undefined);
+    const totalorder = orderData?.data?.meta?.total;
+
+
+
     return (
         <div>
-            <div className="w-full mt-4 px-4 py-3 hover:bg-blue-50 border-blue-50 border-2 rounded-md shadow-md dark:bg-gray-800">
-                <div>
-                    <h1 className="uppercase text-center font-bold">Total user </h1>
-                    <h2 className="text-center text-4xl mb-5 font-bold mt-4">20</h2>
+            <div className="lg:flex gap-10">
+                <div className="w-48 mt-4 px-4 py-3 hover:bg-blue-50 border-blue-50 border-2 rounded-md shadow-md dark:bg-gray-800">
+                    <div>
+                        <h1 className="uppercase text-center font-bold">Total User </h1>
+                        <h2 className="text-center text-4xl mb-5 font-bold mt-4">{totalUser}</h2>
+                    </div>
+                </div>
+                <div className="w-48 mt-4 px-4 py-3 hover:bg-blue-50 border-blue-50 border-2 rounded-md shadow-md dark:bg-gray-800">
+                    <div>
+                        <h1 className="uppercase text-center font-bold">Total Order </h1>
+                        <h2 className="text-center text-4xl mb-5 font-bold mt-4">{totalorder}</h2>
+                    </div>
                 </div>
             </div>
         </div>
--- src/dashboard/OverviewCustomer.tsx
+++ src/dashboard/OverviewCustomer.tsx
@@ -1,14 +1,17 @@
+import { useGetMeOrdersQuery } from "@/redux/features/orderManagmentApi/OrderManagmentApi";
 
 
 
 const OverviewCustomer = () => {
+    const { data: orderMeData } = useGetMeOrdersQuery(undefined);
+    const totalMyOrder = orderMeData?.data?.meta?.total;
 
     return (
         <div>
             <div className="w-full mt-4 px-4 py-3 hover:bg-blue-50 border-blue-50 border-2 rounded-md shadow-md dark:bg-gray-800">
                 <div>
-                    <h1 className="uppercase text-center font-bold">Total order </h1>
-                    <h2 className="text-center text-4xl mb-5 font-bold mt-4">20</h2>
+                    <h1 className="uppercase text-center font-bold">My Total order </h1>
+                    <h2 className="text-center text-4xl mb-5 font-bold mt-4">{totalMyOrder}</h2>
                 </div>
             </div>
         </div>
