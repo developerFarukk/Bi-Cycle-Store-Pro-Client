c44cfd86d40c586ab0c14b9bba49f64ccb5ceecc
FARUK2059 <omarfaruk34552@gmail.com>
2025-02-15 19:24:56 +0600

    update deshboard

--- src/dashboard/OverviewCustomer.tsx
+++ src/dashboard/OverviewCustomer.tsx
@@ -5,7 +5,12 @@ const OverviewCustomer = () => {
 
     return (
         <div>
-            <div> The Component is  Customer Overview </div>
+            <div className="w-full mt-4 px-4 py-3 hover:bg-blue-50 border-blue-50 border-2 rounded-md shadow-md dark:bg-gray-800">
+                <div>
+                    <h1 className="uppercase text-center font-bold">Total order </h1>
+                    <h2 className="text-center text-4xl mb-5 font-bold mt-4">20</h2>
+                </div>
+            </div>
         </div>
     );
 };
